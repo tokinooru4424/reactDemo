7a395c1358c713fd1b705770dc6a85b7d3e1ea65
tokinooru4424 <minhduc2465@gmail.com>
2024-11-02 01:39:34 +0700

    chapter 5 lesson 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        Test link
+        <div>
+          <button>
+            <Link to={`/users`}>Go to user page</Link>
+          </button>
+          <button>
+            <Link to={`/admins`}>Go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
