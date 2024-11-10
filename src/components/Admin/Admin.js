9d941e921c6f9649ef659bc8fe18898fdedb5de9
tokinooru4424 <minhduc2465@gmail.com>
2024-11-10 07:59:18 +0700

    chapter 6 lesson 8

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,6 +2,9 @@ import SideBar from "./SideBar";
 import "./Admin.scss";
 import { FaBars } from "react-icons/fa";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Admin = (props) => {
   const [collapsed, setCollapsed] = useState(false);
@@ -12,9 +15,25 @@ const Admin = (props) => {
         <SideBar collapsed={collapsed} />
       </div>
       <div className="admin-content">
-        <FaBars onClick={() => setCollapsed(!collapsed)} />
-        content goes here
+        <div className="admin-header">
+          <FaBars onClick={() => setCollapsed(!collapsed)} />
+        </div>
+        <div className="admin-main">
+          <Outlet></Outlet>
+        </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <ToastContainer />
     </div>
   );
 };
